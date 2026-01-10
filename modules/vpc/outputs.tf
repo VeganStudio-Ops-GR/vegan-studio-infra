e2cc26292ebed36eb6002dbbe7eef19227f7909f
Raj <gururajrathod90@gmail.com>
2026-01-10 23:25:45 +0530

    VS-109: Removed duplicate vpc_id output

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -1,42 +1,19 @@
+# 1. VPC ID
 output "vpc_id" {
-  description = "The ID of the VPC"
-  value       = aws_vpc.main.id
+  value = aws_vpc.main.id
 }
 
-output "public_subnets" {
-  description = "List of IDs of public subnets"
-  value       = aws_subnet.public[*].id
-}
-
-output "app_subnets" {
-  description = "List of IDs of private application subnets"
-  value       = aws_subnet.private_app[*].id
-}
-
-output "data_subnets" {
-  description = "List of IDs of private data subnets"
-  value       = aws_subnet.private_data[*].id
-}
-
-output "vpc_cidr" {
-  description = "The CIDR block of the VPC"
-  value       = aws_vpc.main.cidr_block
-}
-
-output "data_subnet_ids" {
-  value = aws_subnet.private_data[*].id
-}
-
-# For App Layer (if needed later)
-output "app_subnet_ids" {
-  value = aws_subnet.private_app[*].id
-}
-
-# For Public Layer (ALB needs this)
+# 2. Public Subnets (for ALB)
 output "public_subnet_ids" {
   value = aws_subnet.public[*].id
 }
 
-output "vpc_id" {
-  value = aws_vpc.main.id
+# 3. App Subnets (for EC2 Application)
+output "app_subnet_ids" {
+  value = aws_subnet.private_app[*].id
+}
+
+# 4. Data Subnets (for RDS Database)
+output "data_subnet_ids" {
+  value = aws_subnet.private_data[*].id
 }
