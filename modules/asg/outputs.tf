e7509f56bd78dd6f371add056ea58de5ae7f8881
Raj <gururajrathod90@gmail.com>
2026-01-11 00:50:41 +0530

    VS-110: Fixed wrong outputs in ASG module

--- modules/asg/outputs.tf
+++ modules/asg/outputs.tf
@@ -1,11 +1,3 @@
-output "alb_sg_id" {
-  value = aws_security_group.alb_sg.id
-}
-
-output "app_sg_id" {
-  value = aws_security_group.app_sg.id
-}
-
-output "db_sg_id" {
-  value = aws_security_group.db_sg.id
+output "asg_name" {
+  value = aws_autoscaling_group.app_asg.name
 }
