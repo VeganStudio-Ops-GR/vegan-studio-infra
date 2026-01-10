63fa9802bbcc405d78e8648aeaf7638dc088f92f
Raj <gururajrathod90@gmail.com>
2026-01-10 19:16:28 +0530

    VS-106: Added Prod environment with isolated state and unique CIDR

---
+++ env/prod/variables.tf
@@ -0,0 +1,35 @@
+variable "aws_region" {
+  description = "Region to deploy"
+  type        = string
+  default     = "ap-south-1"
+}
+
+variable "project_name" {
+  description = "Project Name"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "VPC CIDR"
+  type        = string
+}
+
+variable "public_subnets_cidr" {
+  description = "Public Subnets"
+  type        = list(string)
+}
+
+variable "app_subnets_cidr" {
+  description = "App Subnets"
+  type        = list(string)
+}
+
+variable "data_subnets_cidr" {
+  description = "Data Subnets"
+  type        = list(string)
+}
+
+variable "availability_zones" {
+  description = "AZs"
+  type        = list(string)
+}
