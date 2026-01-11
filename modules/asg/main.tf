3cd7b45616837a57f46672a3c19c62ec0256be96
Raj <gururajrathod90@gmail.com>
2026-01-12 00:57:05 +0530

    fix: cross-account dns with aliased provider and data lookups

--- modules/asg/main.tf
+++ modules/asg/main.tf
@@ -1,33 +1,31 @@
-# 1. Get the latest Amazon Linux 2 AMI
+# 1. Get the latest Amazon Linux 2023 AMI
 data "aws_ami" "amazon_linux" {
   most_recent = true
   owners      = ["amazon"]
   filter {
     name   = "name"
-    values = ["al2023-ami-2023.*-x86_64"] # <--- Updated to AL2023
+    values = ["al2023-ami-2023.*-x86_64"]
   }
 }
 
-# 2. The Launch Template (The Blueprint)
+# 2. The Launch Template
 resource "aws_launch_template" "app_lt" {
   name_prefix   = "${var.project_name}-lt"
   image_id      = data.aws_ami.amazon_linux.id
   instance_type = "t3.micro"
 
-  # Attach the IAM Profile so EC2 can talk to Secrets Manager
   iam_instance_profile {
     name = var.iam_instance_profile
   }
 
-  # Security Group (Allow HTTP from ALB)
   vpc_security_group_ids = [var.app_sg_id]
 
-  # INJECT THE USER DATA SCRIPT
-  # We read the file and replace the ${variables} with real values
+  # THE FIX: Added env_message to the variables list below
   user_data = base64encode(templatefile("${path.module}/userdata.sh", {
     secret_name = var.secret_name
     region      = "ap-south-1"
     db_endpoint = var.db_endpoint
+    env_message = var.env_message
   }))
 
   tag_specifications {
@@ -38,20 +36,19 @@ resource "aws_launch_template" "app_lt" {
   }
 }
 
-# 3. The Auto Scaling Group (The Manager)
+# 3. The Auto Scaling Group
 resource "aws_autoscaling_group" "app_asg" {
   name                = "${var.project_name}-asg"
   desired_capacity    = 2
   max_size            = 2
   min_size            = 1
-  vpc_zone_identifier = var.private_subnet_ids # Launch in Private App Subnets
+  vpc_zone_identifier = var.private_subnet_ids
 
   launch_template {
     id      = aws_launch_template.app_lt.id
     version = "$Latest"
   }
 
-  # Connect to the Load Balancer (We will add this variable later)
   target_group_arns = [var.target_group_arn]
 
   tag {
